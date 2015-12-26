c5537ebd9b2c2407c8ba02d11d73ce0bc0153557
Koji Arai <jca02266@gmail.com>
2015-12-26 13:14:08 +0900

    Add sample code to generate with the avatar-generator
    It is necessary the "convert" program as "convert-image" derived from the Image magick.

--- app.js
+++ app.js
@@ -2,12 +2,30 @@
 
 var express = require('express');
 var fs = require('fs');
+var path = require('path');
 
 var app = express();
+
+var avatar = require('avatar-generator')({
+  order: 'background face clothes head hair eye mouth'.split(' '),
+  images: path.join(__dirname, 'node_modules/avatar-generator/img'),
+  convert: 'convert-image'
+});
+
+avatar('test@example.com', 'male', 200)
+  .write('./test.jpg', function (err) {
+    if (err) {
+      console.log(err);
+    }
+  });
 
 app.get('/:id', function(req, res) {
   res.header("Content-Type", "image/jpeg");
   fs.readFile('test.jpg', function (err,data) {
+    if (err) {
+      console.log(err);
+      return;
+    }
     res.send(data);
   });
 });
